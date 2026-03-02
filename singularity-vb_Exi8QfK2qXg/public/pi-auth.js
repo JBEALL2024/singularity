5f5fc7ca318f86207119c89acdb41ab47eca8f04
JB2024 <jbdroneaviation@gmail.com>
2026-03-02 11:37:25 -0600

    Add Pi authentication and payment handling

---
+++ singularity-vb_Exi8QfK2qXg/public/pi-auth.js
@@ -0,0 +1,15 @@
+const Pi = window.Pi;
+Pi.init({ version: "2.0" });
+
+function onIncompletePaymentFound(payment) {
+  console.log("Incomplete payment found:", payment);
+}
+
+Pi.authenticate(['payments', 'username'], onIncompletePaymentFound)
+  .then(function (auth) {
+    alert("Hello " + auth.user.username + "!");
+  })
+  .catch(function (error) {
+    console.error("Authentication failed:", error);
+  });
+
