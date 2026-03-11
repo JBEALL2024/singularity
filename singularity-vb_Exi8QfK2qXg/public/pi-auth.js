78f761f085bdfb658ab5af8b5fc5297885483ea2
JB2024 <jbdroneaviation@gmail.com>
2026-03-11 09:54:10 -0500

    Format Pi.init parameters for better readability

--- singularity-vb_Exi8QfK2qXg/public/pi-auth.js
+++ singularity-vb_Exi8QfK2qXg/public/pi-auth.js
@@ -1,5 +1,7 @@
 const Pi = window.Pi;
-Pi.init({ version: "2.0, sandbox: true });
+Pi.init({ version: "2.0", 
+         sandbox: true 
+        });
 
 function onIncompletePaymentFound(payment) {
   console.log("Incomplete payment found:", payment);
